32274055b4c2983b0de5a07bcc5ea0a1e6535ad0
DebJamA <119551158+DebJamA@users.noreply.github.com>
2023-05-30 23:09:01 -0400

    added main project file and routes

---
+++ intern1/views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+
+
+views = Blueprint("views", __name__)
+
+
+@views.route("/")
+def index():
+   return "Posts Page"
+
+
+""" temporarily commented out so app will run
+@views.route('/post/<int: post_id>/')
+def user_profile(post_id):
+   return "Single Page"
+"""
