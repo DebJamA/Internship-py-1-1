530f448dbd279e45520f3cc49d5025d52d309254
DebJamA <119551158+DebJamA@users.noreply.github.com>
2023-07-06 00:00:08 -0400

    Update views.py

--- intern1/views.py
+++ intern1/views.py
@@ -1,16 +1,21 @@
 from flask import Blueprint
 
-
 views = Blueprint("views", __name__)
 
+app = Flask(__name__)
 
-@views.route("/")
-def index():
-   return "Posts Page"
+@views.route ('/')
+def index ():
+    return 'List of Articles' \
+           '<br>' \
+           'Cant Click Article' \
+           '<br>' \
+           '<a href="/testing/int:post_id">Click This Fake Article</a>' \
+           '<br>' \
+           'Cant Go To Article'
 
-
-""" temporarily commented out so app will run
-@views.route('/post/<int: post_id>/')
-def user_profile(post_id):
-   return "Single Page"
-"""
+@views.route ('/article/int:post_id')
+def article_single ():
+    return 'This Fake Article' \
+           '<br>' \
+           'This Fake Article Analysis'
