fbf9889609597dd077a4164ba3379c60e6b498cb
DebJamA <119551158+DebJamA@users.noreply.github.com>
2023-07-13 16:06:54 -0400

    Update __init__.py

--- intern1/__init__.py
+++ intern1/__init__.py
@@ -1,19 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from os import path
-from flask_login import login_manager
-
 
 def create_app():
    app = Flask(__name__)
-   app.config['SECRET_KEY'] = "helloworld"
-
+   app.config['SECRET_KEY'] = "internshipsimcoblog"
 
 # register blueprints with Flask app
    from .views import views
 
-
    app.register_blueprint(views, url_prefix="/")
-
 
    return app
